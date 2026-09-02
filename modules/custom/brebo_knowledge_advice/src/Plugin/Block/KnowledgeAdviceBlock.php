2da7d90c9f3753de5985b0796457c01a50ce4bb8
John Boon <308669014+JohnBN1970@users.noreply.github.com>
2026-09-02 16:12:58 +0200

    Herstel Kennis & advies als publieke kennisroute (#56)
    * Herstel BREBO Kennis & advies module
    
    * Registreer Kennis & advies template
    
    * Voeg Kennis & advies styling library toe
    
    * Herstel Kennis & advies block plugin
    
    * Bouw Kennis & advies klantreis
    
    * Style Kennis & advies klantreis

---
+++ modules/custom/brebo_knowledge_advice/src/Plugin/Block/KnowledgeAdviceBlock.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\brebo_knowledge_advice\Plugin\Block;
+
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+#[Block(
+  id: 'brebo_knowledge_advice_block',
+  admin_label: new TranslatableMarkup('BREBO – Kennis & advies'),
+  category: new TranslatableMarkup('BREBO')
+)]
+final class KnowledgeAdviceBlock extends BlockBase {
+  public function build(): array {
+    return [
+      '#theme' => 'brebo_knowledge_advice',
+      '#attached' => ['library' => ['brebo_knowledge_advice/page']],
+      '#cache' => ['max-age' => -1],
+    ];
+  }
+}
